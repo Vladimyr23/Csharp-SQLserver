44a9fd0d7e95eca41f5e81c6cd425fe7fcbeba37
Vladimyr23 <vladimyr.yes@gmail.com>
2020-03-10 11:28:21 +0000

    main window fix commit

---
+++ Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_SQLserver
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //TO-DO:Check username and password from login
+            this.Hide();
+            StockControlMain main = new StockControlMain();
+            main.Show();
+        }
+    } 
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Csharp_SQLserver
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
